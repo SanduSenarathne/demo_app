26661943f99960e98ee8a805642436095017942a
sanduni <sandunissts@gmail.com>
2023-06-29 16:44:13 +0530

    Create the chat display page

--- lib/View/chatPage.dart
+++ lib/View/chatPage.dart
@@ -12,16 +12,62 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Loading Chat Page',
-          style: TextStyle(
-            color: Color.fromARGB(255, 183, 181, 181),
+    List<Map<String, dynamic>> personData = [
+      {
+        'name': 'Person 1',
+        'icon': Icons.person,
+      },
+      {
+        'name': 'Person 2',
+        'icon': Icons.person,
+      },
+      {
+        'name': 'Person 3',
+        'icon': Icons.person,
+      },
+    ];
+
+    return Container(
+      color: Colors.black,
+      child: Scrollbar(
+        child: ListView.separated(
+          separatorBuilder: (context, child) => Divider(
+            height: 1,
           ),
+          padding: EdgeInsets.all(0.0),
+          itemCount: personData.length,
+          itemBuilder: (context, i) {
+            final person = personData[i];
+            return Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.black,
+              child: Row(
+                children: [
+                  SizedBox(width: 16),
+                  CircleAvatar(
+                    radius: 30,
+                    child: Icon(
+                      person['icon'],
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Text(
+                    person['name'],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
-      backgroundColor: Colors.black,
     );
   }
 }
