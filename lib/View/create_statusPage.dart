bff60af2c23d2d3418fca9a444ed671d3a1628ad
sanduni <sandunissts@gmail.com>
2023-07-11 09:48:05 +0530

    edit the status page

--- lib/View/create_statusPage.dart
+++ lib/View/create_statusPage.dart
@@ -6,11 +6,64 @@ class CreateStatusPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Status Page'),
-      ),
-      body: Center(
-        child: Text('Create your status here'),
+      backgroundColor: Colors.blue[400],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.close),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.tag_faces_sharp),
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.color_lens),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  maxLines: null,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Type a status',
+                    hintStyle: TextStyle(
+                      color: Colors.purple[200],
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
